9ba8302a5e65631c2e0e50aa52b740584a8c4f72
Sam <57677512+threesam@users.noreply.github.com>
2025-02-08 18:58:33 -0500

    adds initial desktop styling for home component

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -12,6 +12,8 @@ type Advocate = {
   phoneNumber: string;
 };
 
+const MIN_SEARCH_LENGTH = 2;
+
 export default function Home({ advocates }: { advocates: Advocate[] }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredAdvocates, setFilteredAdvocates] = useState(advocates ?? []);
@@ -54,45 +56,70 @@ export default function Home({ advocates }: { advocates: Advocate[] }) {
   };
 
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span>{searchTerm}</span>
-        </p>
-        <input
-          style={{ border: "1px solid black" }}
-          onChange={handleInputChange}
-        />
-        <button onClick={handleResetSearch}>Reset Search</button>
-      </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+    <main>
+      <header className="flex justify-center items-center py-4 bg-gray-100">
+        <h1 className="text-2xl font-bold lg:text-4xl">Solace Advocates</h1>
+      </header>
+
+      <form className="flex flex-col bg-white w-full py-12">
+        <div className="container mx-auto">
+          <h2 className="text-xl font-bold pb-2">Who are you looking for?</h2>
+          <p className="pb-4">
+            Search for an advocate by name, city, degree, specialties, or years
+            of experience.
+          </p>
+          <div className="relative w-max">
+            <input
+              className="w-64 border-2 border-gray-500 rounded-md p-2"
+              onChange={handleInputChange}
+              placeholder="Search..."
+            />
+            <button
+              className="absolute right-2 top-1/2 -translate-y-1/2 bg-gray-800 text-white px-2 pb-0.5 rounded-full text-sm data-[show=false]:hidden transition-opacity duration-300"
+              data-show={searchTerm.length > MIN_SEARCH_LENGTH}
+              onClick={handleResetSearch}
+            >
+              x
+            </button>
+          </div>
+          <p
+            className="pt-2 data-[show=true]:opacity-100 data-[show=false]:opacity-0 transition-opacity duration-300"
+            data-show={searchTerm.length > MIN_SEARCH_LENGTH}
+          >
+            Results for: <span className="font-bold text-lg">{searchTerm}</span>
+          </p>
+        </div>
+      </form>
+
+      <table className="flex w-full flex-col text-center pb-4 min-w-4xl">
+        <thead className="border-b-2 border-gray-600 w-full">
+          <tr className="grid grid-cols-10 bg-gray-100 *:py-2 *:border-r-2 *:border-gray-300 *:last:border-r-0">
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>City</th>
+            <th>Degree</th>
+            <th className="col-span-4">Specialties</th>
+            <th>Years of Experience</th>
+            <th>Phone Number</th>
+          </tr>
         </thead>
-        <tbody>
+        <tbody className="w-full *:odd:bg-gray-200 *:even:bg-gray-100">
           {filteredAdvocates.map((advocate, index) => {
             return (
-              <tr key={index}>
+              <tr
+                className="grid grid-cols-10 items-center border-gray-300 *:grid *:place-content-center *:h-full *:p-2 *:border-r-2 *:border-gray-300 *:last:border-r-0"
+                key={index}
+              >
                 <td>{advocate.firstName}</td>
                 <td>{advocate.lastName}</td>
                 <td>{advocate.city}</td>
                 <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
+                <td className="text-sm col-span-4">
+                  <ul>
+                    {advocate.specialties.map((s, index) => (
+                      <li key={`${s}-${index}`}>{s}</li>
+                    ))}
+                  </ul>
                 </td>
                 <td>{advocate.yearsOfExperience}</td>
                 <td>{advocate.phoneNumber}</td>
